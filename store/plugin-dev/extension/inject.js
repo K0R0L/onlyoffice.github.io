4df280831f22df733269dc421afb735a660e5ccb
SanDiegoss <alexander.nagaev52@gmail.com>
2022-08-17 18:18:06 +0300

    try

--- store/plugin-dev/extension/inject.js
+++ store/plugin-dev/extension/inject.js
@@ -1,5 +1,5 @@
 (function(window, undefined) {
-	var URL_TO_PLUGIN = "";
+	var URL_TO_PLUGIN = "https://sandiegoss.github.io/onlyoffice.github.io/sdkjs-plugins/content/effects/";
 	
 	var xhrObj = new XMLHttpRequest();
 	xhrObj.open('GET', URL_TO_PLUGIN + "config.json", false);
